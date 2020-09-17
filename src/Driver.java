9c806110579b6f2b99d6ff9c9b857d5315058621
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-09-16 19:48:24 -0500

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+//Write a function in Java that takes a String representation of a binary number as a parameter and returns a String representation of the hexadecimal equivalent.
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
